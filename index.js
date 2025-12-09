a63062aef6a35fe225a58da9f7c167b2b8ccd274
NiC <prabhatsoft123@gmail.com>
2025-12-09 13:57:23 +0530

    add test three

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Welcome to your first Node project! check git again chec");
+    res.end("Test three");
 });
 
 server.listen(3000, () => {
