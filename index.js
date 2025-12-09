114f150b3b6be6cbd5b640a815c95a783cda6c1a
NiC <prabhatsoft123@gmail.com>
2025-12-09 13:39:20 +0530

    add new for ci/cd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Welcome to your first Node project! check git");
+    res.end("Welcome to your first Node project! check git again chec");
 });
 
 server.listen(3000, () => {
