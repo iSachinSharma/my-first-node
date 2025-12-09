a5747c4aded2519460edb1f5eb92c64e19b9a66f
NiC <prabhatsoft123@gmail.com>
2025-12-09 12:48:52 +0530

    Node app CI/CD setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Welcome to your first Node project!");
+    res.end("Welcome to your first Node project! check git");
 });
 
 server.listen(3000, () => {
