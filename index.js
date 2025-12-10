18ed523210ecc8ccc444c72ac6a6372a8fe387f8
NiC <prabhatsoft123@gmail.com>
2025-12-10 13:03:31 +0530

    add 11

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Test 10");
+    res.end("Test 11");
 });
 
 server.listen(3000, () => {
