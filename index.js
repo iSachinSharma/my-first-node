a9506d2ce054902ce714272b2af3cd0bf7d74d70
NiC <prabhatsoft123@gmail.com>
2025-12-10 12:23:36 +0530

    Add seven

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Test Six");
+    res.end("Test Seven");
 });
 
 server.listen(3000, () => {
